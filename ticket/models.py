9c19474c969ce7e41c144892c21eecb276259973
Satrap18 <kreza6517@gmail.com>
2025-08-20 12:00:00 +0330

    add tikcet models

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Tikcet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    Company_name = models.CharField(max_length=25)
+    date = models.DateTimeField(auto_now_add=True)
+    comments = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name} {self.lastname}"
