d8720ea64a632d51e0c5258e200e2ce35315750e
Satrap18 <kreza6517@gmail.com>
2025-08-21 12:00:00 +0330

    add viewsets for ticket view

--- ticket/views.py
+++ ticket/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from ticket.models import TicketModel
 from rest_framework import status
 from rest_framework.request import Request
-from rest_framework.viewsts import Viewsts
-# from ticket.seriailzers import Ticket
+from rest_framework import viewsets
+from ticket.serializers import TicketSerializer
 # Create your views here.
+
+class TicketViews(viewsets.ModelViewSet):
+
+    queryset = TicketModel.objects.all()
+    serializer_class = TicketSerializer
