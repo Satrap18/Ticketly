4f42eb3e7bceac3349e5a9a2a09010ff0ebab8a9
Satrap18 <kreza6517@gmail.com>
2025-08-21 12:00:00 +0330

    add DefaultRouter

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from ticket.views import TicketViews
+
+router = DefaultRouter()
+router.register('ticket', TicketViews)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
