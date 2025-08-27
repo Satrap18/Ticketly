ae4ac12fd0de5af68a0e68adaad8112680bc3649
Satrap18 <kreza6517@gmail.com>
2025-08-27 12:00:00 +0330

    add urls answer ticket

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ticket.views import TicketViews
+from ticket.views import TicketViewSet, AnswerTicketViewSet
 
 router = DefaultRouter()
-router.register('', TicketViews)
+router.register('tickets', TicketViewSet, basename="tickets")
+router.register('answers', AnswerTicketViewSet, basename="answers")
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
