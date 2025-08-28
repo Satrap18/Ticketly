916dff2ea505dd020e76656670cc3c79a970caf4
Satrap18 <kreza6517@gmail.com>
2025-08-28 14:39:34 +0330

    better model admin

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from ticket.models import TicketModel
+from ticket.models import TicketModel, AnswerTicketModel
 # Register your models here.
 
-admin.site.register(TicketModel)
+@admin.register(TicketModel)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "name", "lastname", "email", "Company_name")
+    search_fields = ("user__username", "name", "lastname", "email", "Company_name")
+    list_filter = ["Company_name"]
+    # inlines = [AnswerTicketInline]
+
+
+@admin.register(AnswerTicketModel)
+class AnswerTicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "ticket", "subject", "created_at")
+    search_fields = ("subject", "message_text", "ticket__user__username")
+    list_filter = ["created_at"]
