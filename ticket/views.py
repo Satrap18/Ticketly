0dbe517c5625b9582c9f5879b3c105b88405e7da
Satrap18 <kreza6517@gmail.com>
2025-08-28 15:23:09 +0330

    add viewset for answer ticket

--- ticket/views.py
+++ ticket/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from ticket.models import TicketModel
+from ticket.models import TicketModel, AnswerTicketModel
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework import viewsets
-from ticket.serializers import TicketSerializer
+from ticket.serializers import TicketSerializer, AnswerTicketSerializer
 # Create your views here.
 
-class TicketViews(viewsets.ModelViewSet):
+class TicketViewSet(viewsets.ModelViewSet):
 
     queryset = TicketModel.objects.all()
     serializer_class = TicketSerializer
+
+
+class AnswerTicketViewSet(viewsets.ModelViewSet):
+
+    queryset = AnswerTicketModel.objects.all()
+    serializer_class = AnswerTicketSerializer
