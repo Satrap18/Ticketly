e2ef5edfd7ef384347cf85ddb07542df118c90da
Satrap18 <kreza6517@gmail.com>
2025-08-28 14:39:20 +0330

    add Answer Ticket Model

--- ticket/models.py
+++ ticket/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class TicketModel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tickets")
     name = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
     email = models.EmailField()
@@ -17,3 +17,12 @@ class TicketModel(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.name} {self.lastname}"
+
+class AnswerTicketModel(models.Model):
+    ticket = models.ForeignKey(TicketModel, on_delete=models.CASCADE, related_name="answers")
+    subject = models.CharField(max_length=50)
+    message_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Answer by {self.message_text} to {self.ticket}"
