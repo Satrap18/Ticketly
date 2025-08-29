7bd7c8d21b92fcb329a9759f7683a8f0b40b9a5a
Satrap18 <kreza6517@gmail.com>
2025-08-29 19:53:58 +0330

    add url set telegram id

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from accounts.views import RegisterUserView
+from accounts.views import RegisterUserView, SetTelegramIDView
 
 router = DefaultRouter()
 router.register(r'users', RegisterUserView, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('set-telegram-id/', SetTelegramIDView.as_view(), name='set_telegram_id'),
 
 ]
