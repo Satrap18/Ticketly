6c463c58081a1c0540f36c7aa4a90d3627d5d413
Satrap18 <kreza6517@gmail.com>
2025-08-29 13:02:01 +0330

    add basic button

--- bot/bot.py
+++ bot/bot.py
@@ -10,21 +10,57 @@ import django
 django.setup()
 
 from accounts.models import CustomUserModel
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, ConversationHandler, filters, Application,CallbackContext
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 TOKEN = "7077726700:AAEQVdll2qPcUoGebLHjBPa00tA6J_puYns"
 
+ACCOUNT = range(1)
+
 async def start(update, context):
-    await update.message.reply_text("Hello! The robot turned on")
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    fullname = update.message.from_user.full_name 
+    await update.message.reply_text(f"Hello {fullname}, welcome to the ticket robot")
+    keyboard = [['Login'], ['Signup']]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await context.bot.send_message(chat_id=chat_id,
+                                   text='Please choose:',
+                                   reply_markup=reply_markup)
+
+    return ACCOUNT
+
+async def account(update, context):
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+
+    if update.message.text == 'Login':
+        await context.bot.send_message(chat_id, 'You Select Login')
+    elif update.message.text == 'Signup':
+        await context.bot.send_message(chat_id, 'You Select Signup')
+    else:
+        await context.bot.send_message(chat_id, 'Please Select True Options!')
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text('cancelled!')
+    return ConversationHandler.END
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler("start", start))
-
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            ACCOUNT: [MessageHandler(filters.TEXT & ~filters.COMMAND, account)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+        )
+    
+    application.add_handler(conv_handler)
     application.run_polling()
 
 if __name__ == '__main__':
