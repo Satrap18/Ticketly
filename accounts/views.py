087180397384a0a3526fb17f344aabde2c1f27df
Satrap18 <kreza6517@gmail.com>
2025-08-29 19:53:47 +0330

    add set telegram id view

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 User = get_user_model()
@@ -17,3 +18,15 @@ class RegisterUserView(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializers
+
+
+class SetTelegramIDView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        telegram_id = request.data.get("telegram_id")
+        if telegram_id:
+            request.user.telegram_id = telegram_id
+            request.user.save()
+            return Response({"status": "ok"})
+        return Response({"status": "missing telegram_id"}, status=400)
