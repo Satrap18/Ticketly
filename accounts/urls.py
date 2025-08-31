6168a1fa9f8a13c60ac97768ff28975ca92c7cc2
Satrap18 <kreza6517@gmail.com>
2025-08-31 23:09:07 +0330

    add profile urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from accounts.views import RegisterUserView, SetTelegramIDView
+from accounts.views import RegisterUserView, SetTelegramIDView, UserProfileView
 
 router = DefaultRouter()
 router.register(r'users', RegisterUserView, basename='user')
@@ -8,5 +8,8 @@ router.register(r'users', RegisterUserView, basename='user')
 urlpatterns = [
     path('', include(router.urls)),
     path('set-telegram-id/', SetTelegramIDView.as_view(), name='set_telegram_id'),
+    path('profile/', UserProfileView.as_view(), name='get_profile'),
+
+    
 
 ]
