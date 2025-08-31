d6c5b6d7693ae6aee0c6376809006027c3d7b959
Satrap18 <kreza6517@gmail.com>
2025-08-31 23:09:35 +0330

    add user profile view (apiview)

--- accounts/views.py
+++ accounts/views.py
@@ -37,3 +37,16 @@ class SetTelegramIDView(APIView):
         obj.save()
 
         return Response({"status": "ok"})
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email
+        })
