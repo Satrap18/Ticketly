4d4bb05389b661fd848b18859578802a6a215b10
Satrap18 <kreza6517@gmail.com>
2025-09-04 12:00:00 +0330

    add SwaggerView

--- website/urls.py
+++ website/urls.py
@@ -17,12 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('auth-token/', obtain_auth_token, name='generate_auth_token'),
     path('ticket/', include('ticket.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
     
 ]
